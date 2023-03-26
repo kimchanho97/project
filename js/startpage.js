986eea5d5b7e30d9f663ae1648f786e549dea40b
kimchanho97 <nh0903@pusan.ac.kr>
2023-03-27 00:32:13 +0900

    Update program, apply, information: add content

--- js/startpage.js
+++ js/startpage.js
@@ -1,6 +1,7 @@
+// click toggle btn
 const btn = document.querySelector('.navbar_btn');
 const navbar = document.querySelector('.navbar');
 
 btn.addEventListener('click', () => {
     navbar.classList.toggle('active');
-}); // 지정하고 있는 함수 호출
+}); 
