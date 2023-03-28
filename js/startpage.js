1c32ac1c30759cf1d72ef6dc5349338713220272
kimchanho <nh0903@pusan.ac.kr>
2023-03-29 02:35:35 +0900

    Update js

--- js/startpage.js
+++ js/startpage.js
@@ -7,7 +7,10 @@ btn.addEventListener('click', () => {
 }); 
 
 // Mark out current page
-// tag의 class를 active로 변경하더라도 새로운 html file 파싱시 초기화됨 -> file 다운로드시 selector 변화
+// 초기 방안: a tag가 click 될 때마다 모든 a tag class remove -> active
+// -> 이동 후 html file을 파싱할 때 초기화 되는 문제 발생
+// :처음 html file 다운로드시 selector를 처리해주어야 함
+
 var currentUrl = window.location.href; // const 선언시 재할당 불가
 if (currentUrl.includes("about")){
     currentUrl = "./about.html";
