8d5824ce11d18291a6f2dcccf797dca5f673b40f
kimchanho <nh0903@pusan.ac.kr>
2023-04-01 19:10:05 +0900

    Update program: add content

--- js/startpage.js
+++ js/startpage.js
@@ -1,12 +1,12 @@
-// click toggle btn
-const btn = document.querySelector('.navbar_btn');
+// 1. click toggle btn
+const navbar_btn = document.querySelector('.navbar_btn');
 const navbar = document.querySelector('.navbar');
 
-btn.addEventListener('click', () => {
+navbar_btn.addEventListener('click', () => {
     navbar.classList.toggle('active');
 }); 
 
-// Mark out current page
+// 2. Mark out current page
 // 초기 방안: a tag가 click 될 때마다 모든 a tag class remove -> active
 // -> 이동 후 html file을 파싱할 때 초기화 되는 문제 발생
 // :처음 html file 다운로드시 selector를 처리해주어야 함
@@ -30,3 +30,33 @@ if (currentLink){
     currentLink.classList.add('active');
 }
 
+
+// 3. program 선택 button 활성화
+const muscle_btn = document.querySelector('#muscle_btn');
+const strength_btn = document.querySelector('#strength_btn');
+const muscle_cont = document.querySelector('.track_content_m');
+const strength_cont = document.querySelector('.track_content_s');
+
+// 초기 상태
+muscle_btn.classList.add('active');
+strength_cont.style.display = "none";
+
+muscle_btn.addEventListener('click', function(event){
+    // btn
+    strength_btn.classList.remove('active');
+    muscle_btn.classList.add('active');
+    
+    // content
+    strength_cont.style.display = "none";
+    muscle_cont.style.display = "flex";
+});
+
+strength_btn.addEventListener('click', function(event){
+    // btn
+    muscle_btn.classList.remove('active');
+    strength_btn.classList.add('active');
+    
+    // content
+    muscle_cont.style.display = "none";
+    strength_cont.style.display = "flex";
+});
